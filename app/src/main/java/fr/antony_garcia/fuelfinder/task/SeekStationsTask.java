7ea8a2f6ac7c1fbae180f79915fbfe3497b90a5b
AntonyGarcia <antony.garcia@etu.univ-lyon1.fr>
2022-03-08 12:00:20 +0100

    Construction de la base de SeekStationsTask.java

--- app/src/main/java/fr/antony_garcia/fuelfinder/task/SeekStationsTask.java
+++ app/src/main/java/fr/antony_garcia/fuelfinder/task/SeekStationsTask.java
@@ -1,4 +1,21 @@
 package fr.antony_garcia.fuelfinder.task;
 
-public class SeekStationsTask {
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import fr.antony_garcia.fuelfinder.model.Station;
+
+public class SeekStationsTask extends AsyncTask<String,Void, List<Station>> {
+
+
+    @Override
+    protected List<Station> doInBackground(String... query) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<Station> stations) {
+        super.onPostExecute(stations);
+    }
 }
