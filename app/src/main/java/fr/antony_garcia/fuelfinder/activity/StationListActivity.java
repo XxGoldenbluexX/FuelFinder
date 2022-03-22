bbab2cc2381d759ff9acf248be9110a5c93098de
AntonyGarcia <antony.garcia@etu.univ-lyon1.fr>
2022-03-22 12:02:09 +0100

    grumble

--- app/src/main/java/fr/antony_garcia/fuelfinder/activity/StationListActivity.java
+++ app/src/main/java/fr/antony_garcia/fuelfinder/activity/StationListActivity.java
@@ -1,73 +1,36 @@
 package fr.antony_garcia.fuelfinder.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.navigation.ui.NavigationUI;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.navigation.NavigationView;
 
-import fr.antony_garcia.fuelfinder.EndlessRecyclerViewScrollListener;
 import fr.antony_garcia.fuelfinder.R;
-import fr.antony_garcia.fuelfinder.StationAdapter;
-import fr.antony_garcia.fuelfinder.model.Station;
-import fr.antony_garcia.fuelfinder.task.SeekStationsTask;
 
 public class StationListActivity extends AppCompatActivity {
-
-    private Button btn_submit;
-    private EditText et_query;
-    private RecyclerView rv_stationList;
-    private StationAdapter stationAdapter;
-    private final List<Station> liste = new ArrayList<>();
-    private String lastQuery = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stationlist_activity);
         //ActionBar
-        setSupportActionBar(findViewById(R.id.sla_toolbar));
+        //setSupportActionBar(findViewById(R.id.sla_toolbar));
+        //
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationView navView = findViewById(R.id.sla_nv_drawer);
+        DrawerLayout drawer = findViewById(R.id.sla_dl_root);
         AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(NavControllernavController.getGraph())
-                        .setOpenableLayout(R.id.sla_nv_drawer)
+                new AppBarConfiguration.Builder(navController.getGraph())
+                        .setOpenableLayout(drawer)
                         .build();
-        //Get elements from the layout
-        btn_submit = findViewById(R.id.btn_search);
-        et_query = findViewById(R.id.et_query);
-        rv_stationList = findViewById(R.id.rv_stationList);
-        //Create adapter
-        stationAdapter = new StationAdapter(this,liste);
-        //param RecyclerView
-        rv_stationList.setAdapter(stationAdapter);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        rv_stationList.setLayoutManager(llm);
-        rv_stationList.addOnScrollListener(new EndlessRecyclerViewScrollListener(llm) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                new SeekStationsTask(liste,stationAdapter).execute(
-                        "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&sort=-price_gazole&rows=10&start="+10*page+"&q="+lastQuery
-                );
-            }
-        });
-        //Handle events
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                liste.clear();
-                stationAdapter.notifyDataSetChanged();
-                lastQuery = et_query.getText().toString();
-                new SeekStationsTask(liste,stationAdapter).execute(
-                        "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&q=&sort=-price_gazole&rows=10&q="+lastQuery
-                );//TODO change query with the EditText's value
-            }
-        });
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
     }
 }
---
+++ app/src/main/java/fr/antony_garcia/fuelfinder/fragment/StationListFragment.java
@@ -0,0 +1,68 @@
+package fr.antony_garcia.fuelfinder.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.antony_garcia.fuelfinder.EndlessRecyclerViewScrollListener;
+import fr.antony_garcia.fuelfinder.R;
+import fr.antony_garcia.fuelfinder.StationAdapter;
+import fr.antony_garcia.fuelfinder.model.Station;
+import fr.antony_garcia.fuelfinder.task.SeekStationsTask;
+
+public class StationListFragment extends Fragment {
+
+    private Button btn_submit;
+    private EditText et_query;
+    private RecyclerView rv_stationList;
+    private StationAdapter stationAdapter;
+    private final List<Station> liste = new ArrayList<>();
+    private String lastQuery = "";
+
+    public StationListFragment(){
+        super(R.layout.stationlist_fragment);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Get elements from the layout
+        btn_submit = view.findViewById(R.id.btn_search);
+        et_query = view.findViewById(R.id.et_query);
+        rv_stationList = view.findViewById(R.id.rv_stationList);
+        //Create adapter
+        stationAdapter = new StationAdapter(getActivity(),liste);
+        //param RecyclerView
+        rv_stationList.setAdapter(stationAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv_stationList.setLayoutManager(llm);
+        rv_stationList.addOnScrollListener(new EndlessRecyclerViewScrollListener(llm) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                new SeekStationsTask(liste,stationAdapter).execute(
+                        "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&sort=-price_gazole&rows=10&start="+10*page+"&q="+lastQuery
+                );
+            }
+        });
+        //Handle events
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                liste.clear();
+                stationAdapter.notifyDataSetChanged();
+                lastQuery = et_query.getText().toString();
+                new SeekStationsTask(liste,stationAdapter).execute(
+                        "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&q=&sort=-price_gazole&rows=10&q="+lastQuery
+                );//TODO change query with the EditText's value
+            }
+        });
+    }
+}
