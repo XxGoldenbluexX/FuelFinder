9f11625635ece9ff40a14541910bb966c46f1594
XxGoldenbluexX <tonyg222@outlook.fr>
2022-03-27 20:31:57 +0200

    La carte s'affiche! (bad manner API KEY)

--- app/src/main/java/fr/antony_garcia/fuelfinder/fragment/StationMapFragment.java
+++ app/src/main/java/fr/antony_garcia/fuelfinder/fragment/StationMapFragment.java
@@ -5,9 +5,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +24,8 @@ import fr.antony_garcia.fuelfinder.task.SeekStationsTask;
 
 public class StationMapFragment extends Fragment {
 
+    MapView mapView;
+
     public StationMapFragment(){
         super(R.layout.stationmap_fragment);
     }
@@ -27,5 +33,57 @@ public class StationMapFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //HH
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        MapsInitializer.initialize(mapView.getContext());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mapView!=null){
+            mapView.onStart();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mapView!=null){
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mapView!=null){
+            mapView.onPause();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mapView!=null){
+            mapView.onStop();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mapView!=null){
+            mapView.onDestroy();
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mapView!=null){
+            mapView.onLowMemory();
+        }
     }
 }
